f15f13ee2ce3b8708955e07247e3077b9296b96a
Nelesh Singla <neleshsingla86@gmail.com>
2022-11-15 11:46:46 -0500

    adding readme and plugin for release preparation

--- src/main/java/io/nsingla/constants/SeleniumConstants.java
+++ src/main/java/io/nsingla/constants/SeleniumConstants.java
@@ -9,8 +9,8 @@ import java.util.logging.Level;
 public class SeleniumConstants {
 
     // Constants for Selenium
-    public static final String SELENIUM_GRID_HOST = System.getProperty("seleniumHost", "localhost");
-    public static final String SELENIUM_GRID_PORT = System.getProperty("seleniumPort", "4444");
+    public static final String SELENIUM_GRID_HOST = System.getProperty("gridHost", "localhost");
+    public static final String SELENIUM_GRID_PORT = System.getProperty("gridPort", "4444");
     public static final String APP_URL = System.getProperty("url", "https://www.google.com");
     public static final Level CONSOLE_LOG_LEVEL = Level.parse(System.getProperty("consoleloglevel", Level.OFF.getName()));
     public static final String TEST_MODE = System.getProperty("mode", TestMode.LOCAL.name());
