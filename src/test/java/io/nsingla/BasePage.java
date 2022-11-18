8650de39a5a1f09ccd92f1141b0902e1652ea862
Nelesh Singla <neleshsingla86@gmail.com>
2022-11-18 11:45:55 -0500

    cleanup unused import

--- src/test/java/io/nsingla/BasePage.java
+++ src/test/java/io/nsingla/BasePage.java
@@ -1,13 +1,10 @@
 package io.nsingla;
 
-import io.nsingla.selenium.actions.BrowserActions;
 import io.nsingla.selenium.actions.PageActions;
 import io.nsingla.selenium.actions.WaitActions;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
 public class BasePage extends LoadableComponent<BasePage> {
@@ -18,6 +15,7 @@ public class BasePage extends LoadableComponent<BasePage> {
 
     @FindBy(css ="[name='q']")
     public WebElement searchBox;
+
     public BasePage(WebDriver driver) {
         this.driver = driver;
         pageActions = new PageActions(driver);
